dd90e7f01a48c98362c9fea8ca714a347f8dcab1
Aimee Lynn Ramirez <alramirez1@student.fullsail.edu>
2020-09-20 16:18:01 -0400

    cdn styles on icon on error and success

--- js/form.js
+++ js/form.js
@@ -1,47 +1,71 @@
 "use strict";
 window.onload = ()=>{
-  // to stop it from loading
+  //to replace icon on class error on getting success checkmark//
+  const include = (file) => {
+    let script = document.createElement("script");
+    script.src = file;
+    script.crossorigin = "anonymous";
+    script.defer = true;
+    getScript(script);
+  };
+  const getScript = (script) => {
+    let htmlScript = document.querySelector("head")
+    htmlScript.insertAdjacentElement("beforeend", script);
+  };
+  include("https://kit.fontawesome.com/68ebcc4019.js");
+  //to replace icon on class error on getting success checkmark//
 
-
-let submitForm = (e) =>{
-  e.preventDefault();
+  /*submit form*/
   let form = document.querySelector("form");
-  let getP = form.querySelectorAll("p");
-//   let selectForm = [];
-//   // there is more than one paragraph
-//   for (let i = 0; i < getP.length; i++) {
-//     selectForm = document.forms;
-//   }
-//  console.log("select form: ", selectForm);
- // honestly i know that there is one form if there was more than selectForm could be useful
-  //knowing that there is 1 form with 0-4: 5 in length of nest tags
-  /*
-    0: form
-        0: input#name.required
-        1: input#email.required
-        2: input#phone.required
-        3: textarea#message
-        4: button
-   */
-  //console.log("new form: ", form + ":" + selectForm[0]);
-  //console.log("each form on elements", selectForm[0].elements);
-  let storeEl = form.elements;
-  //console.log(storeEl);
-  let store = [];
-  //push the items into the store[]
-  for (let key in storeEl) {
-    store.push({
-      id: key,
-      item: storeEl[key].value,
-    });
-    // console.log("check: ", store);
-  }
-  //mapping the values getting items in being read.
-  store.map((el) => {
-    if (el.item != "" && el.item != null) {
-      console.log(el.item);
+  let paragraphs = form.querySelectorAll("p");
+  let submitForm = (e) => {
+    // to stop it from loading
+    e.preventDefault();
+    let storeEl = form.elements;
+    //console.log(storeEl);
+    let store = [];
+    //push the items into the store[]
+    for (let key in storeEl) {
+      store.push({
+        id: key,
+        item: storeEl[key].value,
+      });
+      // console.log("check: ", store);
     }
-  });
-}
+    //mapping the values getting items in being read.
+    store.map((el) => {
+      if (el.item != "" && el.item != null) {
+        console.log(el);
+      }
+    });
+    // or if i just want to get the ids
+    let name = document.getElementById("name");
+    let email = document.getElementById("email");
+    let phone = document.getElementById("phone");
+    let message = document.getElementById("message");
+    let banner = document.querySelector("p");
+    //banner.innerHTML = "Please enter the form fields below:";
+    for (let i = 0; i < paragraphs.length; i++) {
+      console.log(paragraphs[i]);
+    }
+    let validateEmail = (e) => {
+      console.log(store[1].item);
+      var emailID = store[1].item;
+      let atpos = emailID.indexOf("@");
+      let dotpos = emailID.lastIndexOf(".");
+
+      if (atpos < 1 || dotpos - atpos < 2) {
+        banner.innerHTML = `<p class='error'><i class="fas fa-exclamation-triangle"></i> Sorry, please enter the right email format</p>`;
+        return false;
+      }
+      banner.innerHTML = `<p class='error' style="color:green; border: 2px solid green;"><i class="fas fa-check-square" style="color:green;"></i> Success</p>`;
+
+      return true;
+    };
+    //validate email
+    validateEmail();
+  };
+
+  //validate name
   addEventListener("submit", submitForm);
 }
